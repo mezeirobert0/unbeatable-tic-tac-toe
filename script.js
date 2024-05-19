fd28da211ddd701f2db6003008f0ba787544820b
mezeirobert0 <antoniu.mezei@stud.ubbcluj.ro>
2024-05-19 15:47:29 +0300

    Add possibility of replay, add some comments to script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Knuth shuffle function
 function shuffleArray(arr) {
     for(let i = arr.length-1; i > 0; i--){
         let j = Math.floor(Math.random() * (i+1));
@@ -10,22 +11,17 @@ function hideSelectionArea() {
     selectionArea.style.display = "none";
 }
 
-function showPlayArea() {
-    var playArea = document.getElementById("play-area");
-    playArea.style.display = "block";
-}
-
 function showSelectionArea() {
     var selectionArea = document.getElementById("selection-area");
     selectionArea.style.display = "block";
 }
 
-function hideMessageArea() {
-    var messageArea = document.getElementById("message-area");
-    messageArea.style.display = "none";
-    document.getElementById("message").innerHTML = "";
+function showPlayArea() {
+    var playArea = document.getElementById("play-area");
+    playArea.style.display = "flex";
 }
 
+// function to prevent the player from making a move while the bot's "thinking"
 function changeBoardAccess(access) {
     var board = document.getElementById("board");
     board.style.pointerEvents = access;
@@ -33,12 +29,32 @@ function changeBoardAccess(access) {
 
 // function for displaying the move
 function putOnBoard(index) {
-    var x = document.getElementById(`space-${index}`);
-    x.innerHTML = `<img src=\"icons/${game.currentState.turn}.svg\">`
+    var space = document.getElementById(`space-${index}`);
+    space.innerHTML = `<img src=\"icons/${game.currentState.turn}.svg\">`
     game.currentState.put(index);
     
     // make spot inaccessible
-    x.style.pointerEvents = "none";
+    space.style.pointerEvents = "none";
+}
+
+// function for resetting the board on the screen
+function resetGame() {
+    changeBoardAccess('auto');
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`space-${i}`).innerHTML = '';
+        document.getElementById(`space-${i}`).style.pointerEvents = 'inherit';
+    }
+
+    document.getElementById("message").innerHTML = '';
+}
+
+// moving selection area after the board and message, for when the player wants to play again
+function moveSelectionArea() {
+    var selectionArea = document.getElementById("selection-area");
+    var rootDiv = document.getElementById("root");
+
+    rootDiv.appendChild(selectionArea);
 }
 
 let State = function () {
@@ -148,45 +164,55 @@ let State = function () {
         if (this.result === 'draw')
             return 0;
 
+        // intializing the value of the state depending on the turn
         let value = this.turn === 'x' ? -1000 : 1000;
-        let emptySpaces = this.getEmptySpaces();
-        shuffleArray(emptySpaces);
 
+        let emptySpaces = this.getEmptySpaces();
+        shuffleArray(emptySpaces); // shuffling array in order to make the Bot's moves less predictable
+
+        // loop that goes through each available moves
         for (let index of emptySpaces) {
             let nextState = new State();
             nextState.assign(this);
             nextState.put(index);
-
+            
+            // recursive call; X is the maximizing player, so it wants the state with the highest score
+            //                 O is the minimizing player, so it will choose the state with the least score
             value = this.turn === 'x' ? Math.max(value, nextState.minimax()) : Math.min(value, nextState.minimax());
         }
 
         return value;
     }
 
+    // function for getting the index of the spot which would make for the next best move
     this.getNextBestMove = function () {
         let bestIndex = undefined;
-        let bestValue = this.turn === 'x' ? -1000 : 1000;
-        let emptySpaces = this.getEmptySpaces();
-        shuffleArray(emptySpaces);
 
+        // intializing the best value depending on the turn
+        let bestValue = this.turn === 'x' ? -1000 : 1000;
+
+        let emptySpaces = this.getEmptySpaces();
+        shuffleArray(emptySpaces); //  shuffling array (again) in order to make the Bot's moves less predictable
+
+        // loop that goes through each available moves
         for (let index of emptySpaces) {
             let nextState = new State();
             nextState.assign(this);
             nextState.put(index);
-            let value = nextState.minimax();
 
-            if (this.turn === 'x') {
-                if (value > bestValue) {
-                    bestValue = value;
-                    bestIndex = index;
-                }
+            // getting the value of each of the next possible states
+            let value = nextState.minimax();
+            
+            // X will choose the move which yields the state with the highest value
+            if (this.turn === 'x' && value > bestValue) {
+                bestValue = value;
+                bestIndex = index;
             }
 
-            else {
-                if (value < bestValue) {
-                    bestValue = value;
-                    bestIndex = index;
-                }
+            // X will choose the move which yields the state with the minimum value
+            else if (this.turn === 'o' && value < bestValue) {
+                bestValue = value;
+                bestIndex = index;
             }
         }
 
@@ -195,16 +221,19 @@ let State = function () {
 }
 
 let Game = function () {
-    this.currentState = new State();
+    this.currentState = undefined;
     this.playerSymbol = '';
     this.botSymbol = '';
+    this.timesPlayed = 0;
 
+    // function for setting up the player's and the bot's symbols
     this.startGame = function (playerSymbol) {
         this.playerSymbol = playerSymbol;
         this.botSymbol = this.playerSymbol === 'x' ? 'o' : 'x';
     }
 }
 
+// the instance of Game used by the script
 let game = new Game();
 
 // Get the buttons
@@ -213,13 +242,39 @@ let selectOButton = document.getElementById('select-o');
 
 // Add event listeners for the player symbol selectors
 selectXButton.addEventListener('click', function () {
-    game.startGame('x');
+    hideSelectionArea();
+
+    game.timesPlayed++;
+    // some actions that only need to be executed once
+    if (game.timesPlayed === 1) {
+        document.getElementById('welcome-message').style.display = 'none'; // we don't need the welcome message anymore
+        showPlayArea();
+        moveSelectionArea();
+    }
+
+    resetGame(); // emptying the board and making spaces accessible
+    game.currentState = new State();
+    game.startGame('x'); // player's symbol is X
+    
     changeBoardAccess("auto");
 });
 
 selectOButton.addEventListener('click', function () {
-    changeBoardAccess("none"); // board in now disabled for the player
-    game.startGame('o');
+    hideSelectionArea();
+
+    game.timesPlayed++;
+    // some actions that only need to be executed once
+    if (game.timesPlayed === 1) {
+        document.getElementById('welcome-message').style.display = 'none'; // we don't need the welcome message anymore
+        showPlayArea();
+        moveSelectionArea();
+    }
+
+    resetGame(); // emptying the board and making spaces accessible
+    game.currentState = new State();
+    game.startGame('o'); // player's symbol is X
+    
+    changeBoardAccess("none"); // board is now disabled for the player
 
     setTimeout(function () {
         putOnBoard(game.currentState.getNextBestMove());
@@ -227,40 +282,55 @@ selectOButton.addEventListener('click', function () {
     }, 1000);
 });
 
+// event listeners for all spaces of the board
+// player makes the move first (on successful click) and then the bot follows
 document.querySelectorAll('td').forEach(function (space) { 
     space.addEventListener('click', function () {
         let index = this.id;
         index = Number(index.slice(-1));
 
-        putOnBoard(index);
+        putOnBoard(index); // player's move
 
+        // checking for a result
         if (game.currentState.result !== 'none') {
+            let message = document.getElementById("message");
+
             if (game.currentState.result === game.botSymbol)
-                document.getElementById("message").innerHTML = "Bot wins!";
+                message.innerHTML = "Bot wins!";
 
             else if (game.currentState.result === game.playerSymbol)
-                document.getElementById("message").innerHTML = "Player wins!";
+                message.innerHTML = "Player wins!";
 
-            else document.getElementById("message").innerHTML = "It's a draw!";
+            else message.innerHTML = "It's a draw!";
+
+            showSelectionArea(); // enable the user to play again
+            document.getElementById("selection-prompt").innerHTML = 'Play again as:';
         }
 
         // now time for the bot's move
         else {
-            changeBoardAccess("none");
+            changeBoardAccess("none"); // board is now disabled for the player
+
+            // setting a 1 second delay to simulate the bot's thinking
             setTimeout(function () {
-                putOnBoard(game.currentState.getNextBestMove());
+                putOnBoard(game.currentState.getNextBestMove()); // bot's move
 
                 if (game.currentState.result !== 'none') {
+                    let message = document.getElementById("message");
+
                     if (game.currentState.result === game.botSymbol)
-                        document.getElementById("message").innerHTML = "Bot wins!";
+                        message.innerHTML = "Bot wins!";
 
                     else if (game.currentState.result === game.playerSymbol)
-                        document.getElementById("message").innerHTML = "Player wins!";
+                        message.innerHTML = "Player wins!";
 
-                    else document.getElementById("message").innerHTML = "It's a draw!";
+                    else message.innerHTML = "It's a draw!";
+
+                    showSelectionArea(); // enable the user to play again
+                    document.getElementById("selection-prompt").innerHTML = 'Play again as:';
                 }
 
-                else changeBoardAccess("auto");
+                else changeBoardAccess("auto"); // board is accessible again
             }, 1000);
         }
     });
